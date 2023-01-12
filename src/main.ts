84ae1ed2618ba1e14553af776dd31ab371e740ff
Rahul Kumar <rahul.kumar@brainvire.com>
2023-01-12 17:33:26 +0530

    commit :: added Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+
+
+
+const config = new DocumentBuilder()
+    .setTitle('Authentication')
+    .setDescription('The Auth API Description ')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT', in: 'header' },
+      'access-token',
+      )
+    // .addBearerAuth('Authorization' , 'header')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
+await app.listen(3000);
 }
 bootstrap();
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -23,7 +23,16 @@ import { RolesGuard } from '../auth/guards/role.guard';
 // import { Roles } from '../auth/guards/roles.decorator';
 import { Role } from 'src/constants/roles.enum';
 import { Roles } from '../auth/guards/roles.decorator';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Auth- Module')
 @Controller('auth')
 export class UsersController {
   constructor(
@@ -31,9 +40,46 @@ export class UsersController {
     private authService: AuthService,
   ) {}
 
+  @ApiOperation({ summary: 'Create  data from this Api' })
+  @ApiResponse({
+    status: 201,
+    description: 'Created ',
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        firstName: {
+          type: 'string',
+          example: 'Nathu',
+          description: "user's first Name",
+        },
+        lastName: {
+          type: 'string',
+          example: 'Kumar',
+          description: "user's last Name",
+        },
+        userName: {
+          type: 'email',
+          example: 'nathu@gmail.com',
+          description: 'username it must be unique',
+        },
+        password: {
+          type: 'string',
+          example: 'password@123',
+          description: 'password contains uppercase , lowercase , number',
+        },
+
+        role: {
+          type: 'enum',
+          example: 'user',
+          description: 'role  to user , admin , sysadmin ',
+        },
+      },
+    },
+  })
   @Post('/create')
   create(@Body() body: CreateUserDto) {
-
     try {
       return this.authService.createUser(body);
     } catch (err) {
@@ -41,11 +87,25 @@ export class UsersController {
     }
   }
 
+  @ApiOperation({ summary: 'login from this Api' })
   @Post('/login')
   login(@Body() body: LoginUserDto) {
     return this.authService.login(body.userName, body.password);
   }
 
+  @ApiOperation({ summary: 'Get all data from this Api' })
+  @ApiResponse({
+    status: 200,
+    description: 'All Data list',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'Forbidden',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server error ',
+  })
   @Get()
   findAll() {
     return this.usersService.findAll();
@@ -56,12 +116,39 @@ export class UsersController {
   //   return this.usersService.findOneBy(+id);
   // }
 
+  @ApiOperation({ summary: 'update  data from this Api' })
+  @ApiParam({
+    name: 'id',
+    type: 'integer',
+    description: 'enter unique Id ',
+    required: true,
+  })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        firstName: {
+          type: 'string',
+          example: 'Nathu',
+          description: "user's first Name",
+        },
+        lastName: {
+          type: 'string',
+          example: 'Kumar',
+          description: "user's last Name",
+        },
+      },
+    },
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
+  @ApiBearerAuth('JWT')
+  // @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiResponse({ status: 200, description: 'This is the User Info ' })
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
@@ -80,13 +167,12 @@ export class UsersController {
       success: HttpStatus.OK,
       message: 'forget Password link sent to mail',
       data,
-    }); 
+    });
   }
 
-
   // @UseGuards(RolesGuard)
-  @Roles(Role.USER , Role.ADMIN)
-  @UseGuards(JwtAuthGuard ,RolesGuard)
+  @Roles(Role.USER, Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Get('/admin')
   async adminRoute(@Request() req) {
     return req.user;
